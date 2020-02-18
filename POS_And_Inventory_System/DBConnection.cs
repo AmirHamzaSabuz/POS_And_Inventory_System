507326973749cca217349b6bcbe87ba08cd8838c
Amir Hamza <hamza_amir@yahoo.com>
2020-02-18 18:49:34 +0600

    Create DBConnection class and connect to MS SQL Server

---
+++ POS_And_Inventory_System/DBConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_And_Inventory_System
+{
+    class DBConnection
+    {
+        public string MyConnection()
+        {
+            string con = @"Data Source=DESKTOP-1NSPU91\MSSQL_HAMZA;Initial Catalog=POS_DEMO_DB;Integrated Security=True";
+            return con;
+        }
+    }
+}
--- POS_And_Inventory_System/Form1.cs
+++ POS_And_Inventory_System/Form1.cs
@@ -7,17 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace POS_And_Inventory_System
 {
     public partial class Form1 : Form
     {
-      
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        DBConnection dBCon = new DBConnection();
 
         public Form1()
         {
             InitializeComponent();
- 
+            cn = new SqlConnection(dBCon.MyConnection());
+            cn.Open();
+            MessageBox.Show("Connected!");
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
